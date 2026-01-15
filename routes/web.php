e6009945e1638b33f73561f2f645b4d3c8e26e91
Maheen Mohamed Ismayeel <ismayeel2023@gmail.com>
2026-01-15 21:12:58 +0530

    Added student CRUD controller and routes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,17 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\StudentController;
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/students', [StudentController::class, 'index']);
+    Route::get('/students/create', [StudentController::class, 'create']);
+    Route::post('/students', [StudentController::class, 'store']);
+    Route::get('/students/{id}/edit', [StudentController::class, 'edit']);
+    Route::put('/students/{id}', [StudentController::class, 'update']);
+    Route::delete('/students/{id}', [StudentController::class, 'destroy']);
+});
+
 
 Route::get('/', function () {
     return view('welcome');
