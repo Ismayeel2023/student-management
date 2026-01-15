aea674be310f107c9b301047fc141f1955d0f228
Maheen Mohamed Ismayeel <ismayeel2023@gmail.com>
2026-01-15 21:41:51 +0530

    Added role based access control for admin and student

--- routes/web.php
+++ routes/web.php
@@ -5,14 +5,17 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StudentController;
 
 Route::middleware(['auth'])->group(function () {
-    Route::get('/students', [StudentController::class, 'index']);
-    Route::get('/students/create', [StudentController::class, 'create']);
-    Route::post('/students', [StudentController::class, 'store']);
-    Route::get('/students/{id}/edit', [StudentController::class, 'edit']);
-    Route::put('/students/{id}', [StudentController::class, 'update']);
-    Route::delete('/students/{id}', [StudentController::class, 'destroy']);
-});
 
+    Route::get('/students', [StudentController::class, 'index']);
+
+    Route::middleware(['admin'])->group(function () {
+        Route::get('/students/create', [StudentController::class, 'create']);
+        Route::post('/students', [StudentController::class, 'store']);
+        Route::get('/students/{id}/edit', [StudentController::class, 'edit']);
+        Route::put('/students/{id}', [StudentController::class, 'update']);
+        Route::delete('/students/{id}', [StudentController::class, 'destroy']);
+    });
+});
 
 Route::get('/', function () {
     return view('welcome');
